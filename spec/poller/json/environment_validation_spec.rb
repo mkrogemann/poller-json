0864a2da3692445d70e14113967af8a485536f65
Markus Krogemann <markus@krogemann.de>
2013-04-13 01:49:58 +0200

    validate that JSON parsing is available

---
+++ spec/poller/json/environment_validation_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'poller/poller_json'
+
+module Poller
+  module JSON
+  	describe 'Environment Validation' do
+  	  it 'can parse JSON?' do
+  	  	json = ::JSON::parse(File.read(File.expand_path(File.join(__FILE__, '../environment_validation.json'))))
+  	  	json.class.should == Hash
+  	  end
+  	end
+
+  end
+end
