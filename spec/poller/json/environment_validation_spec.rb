43aed2026df3dccb1868a5e3274b8dc1e77936fc
Markus Krogemann <markus@krogemann.de>
2013-04-13 03:40:35 +0200

    test more

--- spec/poller/json/environment_validation_spec.rb
+++ spec/poller/json/environment_validation_spec.rb
@@ -6,7 +6,10 @@ module Poller
   	describe 'Environment Validation' do
   	  it 'can parse JSON?', :type => 'integration' do
   	  	json = ::JSON::parse(File.read(File.expand_path(File.join(__FILE__, '../environment_validation.json'))))
+
   	  	json.class.should == Hash
+  	  	json['menu']['value'].should == 'File'
+  	  	json['menu']['popup']['menuitem'].size.should == 3
   	  end
   	end
   end
