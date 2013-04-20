9bc7b05d98f1ca92fd2ae437ccb62608d920bdd3
Markus Krogemann <markus@krogemann.de>
2013-04-20 19:59:46 +0200

    add integration build for Go

---
+++ build/go-integration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+rvm use 1.9.3@mkrogemann-poller-json --create --fuzzy
+bundle
+
+[ -d rspec-integration ] && rm -rf rspec-integration
+bundle exec rspec --tag type:integration --out rspec-integration/rspec.xml --format html --out rspec-integration/rspec.html
+
+exit $?
