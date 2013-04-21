61fd45c6b1aa2812376e9f3aac1873d2fbec15c3
Markus Krogemann <markus@krogemann.de>
2013-04-21 04:06:57 +0200

    looks better now.

--- lib/matchers/json/json_path.rb
+++ lib/matchers/json/json_path.rb
@@ -12,6 +12,7 @@ module Matchers
 
       def value_on_path(json_hash, path)
         raise ArgumentError, "Invalid json path: #{path}" unless json_path_valid?(path)
+        path = translate_to_dot_notation(path)
         current_node = json_hash # start traversal at document root
         path_items(path).each do |path_item|
           element, index = decompose_path_item(path_item)
@@ -24,6 +25,25 @@ module Matchers
         current_node
       end
 
+      # TODO: the regular expressions below will catch very few errors
+      def json_path_valid?(path)
+        notation = path.include?('[\'') ? :bracket : :dot
+        path_regex = /^\$[a-zA-Z0-9_(\[\d\])\.]*[^\.]$/ if notation == :dot
+        path_regex = /^\$[a-zA-Z0-9_(\[\d\])\.\']*[^\.]$/ if notation == :bracket
+        path_regex.match(path)
+      end
+
+      def translate_to_dot_notation(path)
+        path = path.gsub('[\'', '').gsub('\']', '') if path.include?('[\'')
+        path
+      end
+      private :translate_to_dot_notation
+
+      def path_items(path)
+        path[1..-1].split('.') # removing the leading '$'
+      end
+      private :path_items
+
       def fetch(current_node, element, index)
         current_node = current_node.send(:fetch, element)
         current_node = current_node.send(:fetch, index) if index
@@ -31,29 +51,15 @@ module Matchers
       end
       private :fetch
 
-      def path_items(path)
-        path[1..-1].split('.') # removing the leading '$'
-      end
-      private :path_items
-
       # if the path item contains an index (eg 'sample[3]') then the index will be returned
       # if not, second return value will be nil [element,index]
-      # TODO: does two things: decide on syntax and decompose
       def decompose_path_item(path_item)
-        path_item = path_item.sub('[\'', '').sub('\']', '') if path_item.start_with?('[\'')
         element, index = path_item.split('[')
         index = index[0..-2].to_i if index
         [element, index]
       end
       private :decompose_path_item
 
-      # TODO: the regular expressions below will only catch a few of the possible errors
-      def json_path_valid?(path)
-        notation = path.include?('[\'') ? :bracket : :dot
-        path_regex = /^\$[a-zA-Z0-9_(\[\d\])\.]*[^\.]$/ if notation == :dot
-        path_regex = /^\$[a-zA-Z0-9_(\[\d\])\.\']*[^\.]$/ if notation == :bracket
-        path_regex.match(path)
-      end
     end
   end
 end
